0ab5c2a110e423b5a817014d4f4beb67dd96ffac
jin123457 <jin_ox@naver.com>
2023-09-11 15:24:56 +0900

    form invalid,vaild to peer perfomance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,12 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
-        <form className="flex flex-col space-y-2 bg-blue-500 p-5 focus-within:bg-blue-100">
-            <input type="text" required placeholder="Username" className="required: bg-yellow-500 invalid:bg-red-500 valid:bg-teal-500" />
-            <input type="password" required placeholder="Password" />
+        <form className="flex flex-col space-y-2  p-5">
+            <div className="flex flex-col-reverse">
+                <input type="text" required placeholder="Username" className="border p-1 peer border-gray-400 rounded-lg" />
+                <span className="hidden peer-invalid:block peer-invalid:text-red-500">This input in invalid</span>
+                <span className="hidden peer-valid:block peer-valid:text-teal-500">Awesome username</span>
+            </div>
             <input type="submit" value="login" className="bg-white" />
         </form>
     );
