e6cb45a3986f7c9fb7b86df9c83884f1ec182117
jin123457 <jin_ox@naver.com>
2023-09-12 12:58:56 +0900

    chats detail UI page created

--- src/pages/chats/[id].tsx
+++ src/pages/chats/[id].tsx
@@ -1,22 +1,24 @@
 const ChatDetail = () => {
     return (
         <div className="py-10 px-4 space-y-4">
-            {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-                <div key={i}>
-                    <div className="flex items-start space-x-2">
-                        <div className="w-8 h-8 rounded-full bg-slate-400" />
-                        <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">Hi how much are you selling them for?</div>
+            <div className="pb-5">
+                {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+                    <div key={i} className="flex flex-col space-y-2 mb-2 last:mb-0">
+                        <div className="flex items-start space-x-2">
+                            <div className="w-8 h-8 rounded-full bg-slate-400" />
+                            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">Hi how much are you selling them for?</div>
+                        </div>
+                        <div className="flex items-start space-x-2 flex-row-reverse space-x-reverse">
+                            <div className="w-8 h-8 rounded-full bg-slate-400" />
+                            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">I want ￦20,000</div>
+                        </div>
+                        <div className="flex items-start space-x-2">
+                            <div className="w-8 h-8 rounded-full bg-slate-400" />
+                            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">미쳤어</div>
+                        </div>
                     </div>
-                    <div className="flex items-start space-x-2 flex-row-reverse space-x-reverse">
-                        <div className="w-8 h-8 rounded-full bg-slate-400" />
-                        <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">I want ￦20,000</div>
-                    </div>
-                    <div className="flex items-start space-x-2">
-                        <div className="w-8 h-8 rounded-full bg-slate-400" />
-                        <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">미쳤어</div>
-                    </div>
-                </div>
-            ))}
+                ))}
+            </div>
             <div className="fixed w-full mx-auto px-8 bg-white bottom-0 inset-x-0 py-2">
                 <div className="flex relative items-center">
                     <input
