b07ff54e334aa1c7d1efc7123446f0ae9728d95a
jin123457 <jin_ox@naver.com>
2023-09-19 13:04:28 +0900

    Token Logic & Twilo setup

--- src/app/api/users/auth/route.ts
+++ src/app/api/users/auth/route.ts
@@ -1,56 +1,35 @@
 import client from "@libs/server/client";
 import { NextResponse } from "next/server";
 
-export const POST = async (req: Request) => {
-    const res = await req.json();
-    const { phone, email } = res;
-    const payload = phone ? { phone: +phone } : { email };
-    const user = await client.user.upsert({
-        where: {
-            ...payload,
-        },
-        create: {
-            name: "Anonymous",
-            ...payload,
-        },
-        update: {},
-    });
-    console.log(user);
-    // if (email) {
-    //     user = await client.user.findUnique({
-    //         where: {
-    //             email,
-    //         },
-    //     });
-    //     if (user) console.log("found it.");
-    //     if (!user) {
-    //         console.log("did not found. will create.");
-    //         user = await client.user.create({
-    //             data: {
-    //                 name: "Anonymous",
-    //                 email,
-    //             },
-    //         });
-    //     }
-    // }
+interface ResponseType {
+    ok: boolean;
+    [key: string]: any;
+}
 
-    // if (phone) {
-    //     user = await client.user.findUnique({
-    //         where: {
-    //             phone: +phone,
-    //         },
-    //     });
-    //     if (user) console.log("found it.");
-    //     if (!user) {
-    //         console.log("did not found. will create.");
-    //         user = await client.user.create({
-    //             data: {
-    //                 name: "Anonymous",
-    //                 phone: +phone,
-    //             },
-    //         });
-    //     }
-    //     console.log(user);
-    // }
-    return NextResponse.json({ ok: true });
+export const POST = async (req: Request) => {
+    try {
+        const res = await req.json();
+        const { phone, email } = res;
+        const user = phone ? { phone: +phone } : { email };
+        const payload = Math.floor(100000 + Math.random() * 900000) + "";
+        const token = await client.token.create({
+            data: {
+                payload: "1234",
+                user: {
+                    connectOrCreate: {
+                        where: {
+                            ...user,
+                        },
+                        create: {
+                            name: "Anonymous",
+                            ...user,
+                        },
+                    },
+                },
+            },
+        });
+        return NextResponse.json({ ok: true });
+    } catch (err) {
+        return NextResponse.json({ ok: false });
+    }
 };
